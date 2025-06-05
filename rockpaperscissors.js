9a8859678fad37ba6c3a6de824261123167ec0d5
Agnes Bolton <Agnes-B@users.noreply.github.com>
2025-06-05 22:00:58 +0100

    Tidy final code comments

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -11,7 +11,6 @@ let humanChoice = ""
 
 function getComputerChoice() {
     const random = Math.floor(Math.random() * 3)
-    //console.log(random)
 
     if (random == 0) {
         computerChoice = "rock";
@@ -25,7 +24,6 @@ function getComputerChoice() {
     return computerChoice;
 }
 
-// console.log(getComputerChoice());
 
 /*Create a new function named getHumanChoice.
 Use the prompt method to get the user’s input of "rock" "paper" or "scissors".
@@ -33,32 +31,32 @@ Using console.log to test what the function returns*/
 
 function getHumanChoice() {
     humanChoice = prompt("Type in your choice of rock, paper or scissors.")
-    //console.log(humanChoice);
+
     //Make your function’s humanChoice parameter case-insensitive so that players can input “rock”, “ROCK”, “RocK”, or other variations.
     humanChoice = humanChoice.toLowerCase();
-    //console.log(humanChoice);
+
     return humanChoice;
 }
 
-//console.log(getHumanChoice());
+
 
 /*Create two new variables named humanScore and computerScore in the global scope.
 Initialize those variables with the value of 0.*/
 
 let humanScore = 0;
 let computerScore = 0;
-let humanWins = false;
-let computerWins = false
 
 /*Create a new function named playRound with two parameters for playRound: humanChoice and computerChoice.
 
 Use these two parameters to take the human and computer choices as arguments.*/
 
+/*Write the code for your playRound function to console.log a string value representing the round winner, such as: “You lose! Paper beats Rock”.
+Increment the humanScore or computerScore variable based on the round winner.*/
+
 function playRound() {
+
     getHumanChoice();
-    console.log(humanChoice);
     getComputerChoice();
-    console.log(computerChoice);
 
     if (humanChoice == "rock" && computerChoice == "paper") {
         console.log("You lose! Paper beats Rock.");
@@ -89,7 +87,9 @@ function playRound() {
     }
 }
 
+// playing the game 5 times bu calling the playRound() function repeatedly
 function playGame() {
+
     playRound();
     playRound();
     playRound();
@@ -99,10 +99,5 @@ function playGame() {
 
 playGame();
 
+// Displaying final score in the console log.
 console.log(`The final score is ${computerScore} for the computer and ${humanScore} for you.`);
-
-
-
-
-/*Write the code for your playRound function to console.log a string value representing the round winner, such as: “You lose! Paper beats Rock”.
-Increment the humanScore or computerScore variable based on the round winner.*/
