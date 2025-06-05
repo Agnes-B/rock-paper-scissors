ed5611ffc5893dd87c20f644171a0e4cd59526a8
Agnes Bolton <Agnes-B@users.noreply.github.com>
2025-06-05 16:07:00 +0100

    Create function getComputerChoice() returns string

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -1,1 +1,30 @@
-console.log("Hello, world!");
+/* Create a new function named getComputerChoice.
+
+Randomly generate a number between 0 and 2
+
+If the answer is 0 then return "rock"
+
+else if the answer is 2 then "paper"
+
+else return "scissors"
+
+Console.log returned value
+*/
+
+function getComputerChoice() {
+    const random = Math.floor(Math.random() * 3)
+    console.log(random)
+
+    if (random == 0) {
+        return "rock"
+    } 
+    else if (random == 1) {
+        return "paper"
+    }
+    else {
+        return "scissors"
+    }
+
+}
+
+console.log(getComputerChoice());
