e0b6294fbfa509b2090c93e8f10e7f07edab3e3e
Agnes Bolton <Agnes-B@users.noreply.github.com>
2025-06-05 16:14:49 +0100

    Create promt to getHumanChoice() and log

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -1,13 +1,8 @@
 /* Create a new function named getComputerChoice.
-
 Randomly generate a number between 0 and 2
-
 If the answer is 0 then return "rock"
-
-else if the answer is 2 then "paper"
-
+else if the answer is 1 then "paper"
 else return "scissors"
-
 Console.log returned value
 */
 
@@ -24,7 +19,19 @@ function getComputerChoice() {
     else {
         return "scissors"
     }
-
 }
 
 console.log(getComputerChoice());
+
+/*Create a new function named getHumanChoice.
+
+Use the prompt method to get the user’s input of "rock" "paper" or "scissors".
+
+Test what your function returns by using console.log.*/
+
+function getHumanChoice() {
+    const humanChoice = prompt("Type in your choice of rock, paper or scissors.")
+    return humanChoice
+}
+
+console.log(getHumanChoice());
