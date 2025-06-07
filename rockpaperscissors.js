e511a4f27808be3f65dd041e628d264c6384f984
Agnes Bolton <Agnes-B@users.noreply.github.com>
2025-06-07 22:06:17 +0100

    Game with buttons and new game feature

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -1,13 +1,16 @@
-/* Create a new function named getComputerChoice.
-Randomly generate a number between 0 and 2
-If the answer is 0 then return "rock"
-else if the answer is 1 then "paper"
-else return "scissors"
-Console.log returned value
-*/
+// Create a new function named getComputerChoice.
+
 
 let computerChoice = ""
 let humanChoice = ""
+let humanScore = 0
+let computerScore = 0
+const buttons = document.querySelectorAll("button");
+const results = document.createElement("div");
+const roundWinner = document.createElement("div");
+const scoreCard = document.createElement("div");
+const newGameBtn = document.createElement("button");
+newGameBtn.textContent = "Play again!"
 
 function getComputerChoice() {
     const random = Math.floor(Math.random() * 3)
@@ -24,27 +27,22 @@ function getComputerChoice() {
     return computerChoice;
 }
 
+function startGame() {
+    humanScore = 0;
+    computerScore = 0;
+    roundWinner.textContent = "";
+    scoreCard.textContent = "";
+    results.appendChild(roundWinner);
+    results.appendChild(scoreCard);
+    document.body.appendChild(results);
 
-/*Create a new function named getHumanChoice.
-Use the prompt method to get the user’s input of "rock" "paper" or "scissors".
-Using console.log to test what the function returns*/
-
-function getHumanChoice() {
-    humanChoice = prompt("Type in your choice of rock, paper or scissors.")
-
-    //Make your function’s humanChoice parameter case-insensitive so that players can input “rock”, “ROCK”, “RocK”, or other variations.
-    humanChoice = humanChoice.toLowerCase();
-
-    return humanChoice;
 }
-
 
 
 /*Create two new variables named humanScore and computerScore in the global scope.
 Initialize those variables with the value of 0.*/
 
-let humanScore = 0;
-let computerScore = 0;
+
 
 /*Create a new function named playRound with two parameters for playRound: humanChoice and computerChoice.
 
@@ -53,49 +51,91 @@ Use these two parameters to take the human and computer choices as arguments.*/
 /*Write the code for your playRound function to console.log a string value representing the round winner, such as: “You lose! Paper beats Rock”.
 Increment the humanScore or computerScore variable based on the round winner.*/
 
-function playRound() {
+async function updateScore() {
 
-    getHumanChoice();
-    getComputerChoice();
+    scoreCard.textContent = `The score is ${computerScore} for the computer and ${humanScore} for you.`;
 
-    if (humanChoice == "rock" && computerChoice == "paper") {
-        console.log("You lose! Paper beats Rock.");
-        computerScore++;
-    }
-    else if (humanChoice == "paper" && computerChoice == "scissors") {
-        console.log("You lose! Scissors beat Paper.");
-        computerScore++;
-    }
-    else if (humanChoice == "scissors" && computerChoice == "rock") {
-        console.log("You lose! Rock beats Scissors.");
-        computerScore++;
-    }
-    else if (humanChoice == "paper" && computerChoice == "rock") {
-        console.log("You win! Paper beats Rock.");
-        humanScore++;
-    }
-    else if (humanChoice == "scissors" && computerChoice == "paper") {
-        console.log("You win! Scissors beat Paper.");
-        humanScore++;
-    }
-    else if (humanChoice == "rock" && computerChoice == "scissors") {
-        console.log("You win! Rock beats Scissors.");
-        humanScore++;
-        }
-    else {
-        console.log("You both chose the same, it is a DRAW.");
+
+}
+
+async function checkWinner() {
+    await new Promise(resolve => setTimeout(resolve, 200))
+    if (humanScore == 5) {
+        gameOver("Human");
+    } else if (computerScore == 5) {
+        gameOver("Computer");
     }
 }
 
-// playing the game 5 times bu calling the playRound() function repeatedly
-function playGame() {
+startGame();
 
+function playRound(e) {
+
+    humanChoice = e.target.id;
+    getComputerChoice();
+
+    if (humanChoice == "rock" && computerChoice == "paper") {
+        roundWinner.textContent = "You lose! Paper beats Rock.";
+        computerScore++;
+    }
+    else if (humanChoice == "paper" && computerChoice == "scissors") {
+        roundWinner.textContent = "You lose! Scissors beat Paper.";
+        computerScore++;
+    }
+    else if (humanChoice == "scissors" && computerChoice == "rock") {
+        roundWinner.textContent = "You lose! Rock beats Scissors.";
+        computerScore++;
+    }
+    else if (humanChoice == "paper" && computerChoice == "rock") {
+        roundWinner.textContent = "You win! Paper beats Rock.";
+        humanScore++;
+    }
+    else if (humanChoice == "scissors" && computerChoice == "paper") {
+        roundWinner.textContent = "You win! Scissors beat Paper.";
+        humanScore++;
+    }
+    else if (humanChoice == "rock" && computerChoice == "scissors") {
+        roundWinner.textContent = "You win! Rock beats Scissors.";
+        humanScore++;
+        }
+    else {
+        roundWinner.textContent = "You both chose the same, it is a DRAW.";
+    }
+
+
+    updateScore();
+    checkWinner();
+
+}
+
+function gameOver(player) {
+    alert(`Game over! ${player} won.`)
+    buttons.forEach(button => button.disabled);
+    document.body.appendChild(newGameBtn);
+}
+
+function newGame() {
+    document.body.removeChild(results);
+    document.body.removeChild(newGameBtn);
+    startGame();
+}
+
+buttons.forEach(button => button.addEventListener('click', playRound));
+newGameBtn.addEventListener('click', newGame);
+
+
+
+
+
+// playing the game 5 times bu calling the playRound() function repeatedly
+/*
+function playGame() {
     for (let i = 0; i < 5; i++) {
     playRound();
     }
 }
 
 playGame();
+*/
 
 // Displaying final score in the console log.
-console.log(`The final score is ${computerScore} for the computer and ${humanScore} for you.`);
